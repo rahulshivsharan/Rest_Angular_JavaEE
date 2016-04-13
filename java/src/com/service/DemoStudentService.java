6b3c2e5319d455c9810aae13f0d4bb986d1f4276
Rahul Shivsharan <postbox.rahul@hotmail.com>
2016-04-13 17:49:26 +0530

    Update DemoStudentService.java

--- java/src/com/service/DemoStudentService.java
+++ java/src/com/service/DemoStudentService.java
@@ -42,4 +42,33 @@ public class DemoStudentService {
 		
 		return response;
 	}
+	
+		@GET
+	@Path("colleges")
+	@Produces({MediaType.APPLICATION_JSON})
+	public Response getColleges() {
+		List<CollegeEntity> collegeList = null;
+		ObjectMapper mapper = null;
+		String json = null;
+		Response response = null;
+		try{
+			Thread.sleep(4000);
+			collegeList = Arrays.asList(
+					new CollegeEntity("Bharati Vidyapeeth College",123),
+					new CollegeEntity("Shree Swami Samartha College",223),
+					new CollegeEntity("Siddharth College",235),
+					new CollegeEntity("Rizvi College",237),
+					new CollegeEntity("Smt. Nanda Patil College",121),
+					new CollegeEntity("Shree Bhau Saheb Patil College",133));
+			mapper = new ObjectMapper();
+			json = mapper.writeValueAsString(collegeList);
+			response = Response.ok(json).build();
+			//throw new Exception("Some Exception baba");			
+		}catch(Exception e){
+			ErrorResponse errorObj = new ErrorResponse("College services is down"); 
+			response = Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity(errorObj).build();
+		}
+		
+		return response;
+	}
 }
